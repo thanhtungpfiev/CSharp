63174cbcef483b77b147c2f45950dfc5cb8383c6
tung.daothanh <tung.daothanh@vn.bosch.com>
2024-04-06 10:58:34 +0700

    Learn how to create DLL for C++

---
+++ MyProjects/MathDLL/MathDLL/MathLib.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "MathLib.h"
+
+double Add(double a, double b)
+{
+	return a + b;
+}
+
+double Sub(double a, double b)
+{
+	return a - b;
+}
+
+double Mul(double a, double b)
+{
+	return a * b;
+}
+
+double Div(double a, double b)
+{
+	return a / b;
+}
---
+++ MyProjects/MathDLL/UsingMathDLL/include/MathLib.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#ifdef MATHDLL_EXPORTS
+#define MATHDLL_API __declspec(dllexport)
+#else
+#define MATHDLL_API __declspec(dllimport)
+#endif
+
+extern "C" MATHDLL_API double Add(double a, double b);
+
+extern "C" MATHDLL_API double Sub(double a, double b);
+
+extern "C" MATHDLL_API double Mul(double a, double b);
+
+extern "C" MATHDLL_API double Div(double a, double b);
